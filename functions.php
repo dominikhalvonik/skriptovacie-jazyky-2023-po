f2878255ad2adc016443342c369a4ff1c60ab7f2
dominik.halvonik <dominik.halvonik@gmail.com>
2023-03-23 12:26:34 +0100

    MySQL

--- functions.php
+++ functions.php
@@ -2,9 +2,59 @@
 
 namespace ukf;
 
+use PDO;
+
 class Menu
 {
     private $sourceFileName = "source/headerMenu.json";
+
+    private string $host;
+    private int $port;
+    private string $username;
+    private string $password;
+    private string $dbName;
+
+    private $connection;
+
+    public function __construct(
+        string $host = "localhost",
+        int $port = 3306,
+        string $user = "root",
+        string $password = "",
+        string $dbName = "sj-2023-2"
+    )
+    {
+        if(!empty($host)) {
+            $this->host = $host;
+        }
+
+        if(!empty($port)) {
+            $this->port = $port;
+        }
+
+        if(!empty($user)) {
+            $this->username = $user;
+        }
+
+        if(isset($password)) {
+            $this->password = $password;
+        }
+
+        if(!empty($dbName)) {
+            $this->dbName = $dbName;
+        }
+
+        try {
+            $this->connection = new PDO(
+                'mysql:host='.$this->host.';dbname='.$this->dbName.";port=".$this->port,
+                $this->username,
+                $this->password
+            );
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
+            die();
+        }
+    }
 
     public function getMenuData(string $type): array
     {
@@ -13,8 +63,19 @@ class Menu
         if ($this->validateMenuType($type)) {
             if ($type === "header") {
                 try {
-                    $menuJsonFile = file_get_contents($this->sourceFileName);
-                    $menu = json_decode($menuJsonFile, true);
+                    //$menuJsonFile = file_get_contents($this->sourceFileName);
+                    //$menu = json_decode($menuJsonFile, true);
+
+                    $sql = "SELECT * FROM menu";
+                    $query = $this->connection->query($sql);
+                    $menuData = $query->fetchAll(PDO::FETCH_ASSOC);
+
+                    foreach ($menuData as $menuItem) {
+                        $menu[$menuItem['sys_name']] = [
+                            'name' => $menuItem['user_name'],
+                            'path' => $menuItem['path']
+                        ];
+                    }
                 } catch (\Exception $exception) {
                     //echo $exception->getMessage();
                 }
